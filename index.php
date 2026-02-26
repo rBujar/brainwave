b463778ca850d4e90b509aef373b9f83ebc8f36c
Bujar Roka <rokabujar@gmail.com>
2026-02-26 12:52:53 +0100

    setting up the theme

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="/src/css/style.scss">
+    <link rel="shortcut icon" type="image/x-icon" href="src/assets/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Brainwave</title>
+  </head>
+  <body>
+    <header class="container">
+      <div class="header__logo">
+        <img src="src/assets/logo.svg" alt="logo">
+      </div>
+      <div class="header__nav container">
+      <ul class="header__menu container">
+            <li class="header__item"><a href="#">Demos</a></li>
+            <li class="header__item"><a href="#">Pages</a></li>
+            <li class="header__item"><a href="#">Support</a></li>
+            <li class="header__item"><a href="#">Contact</a></li>
+        </ul>
+        <button class="blue-btn">get a free quote</button>
+      </div>
+      <div class="header__hamburger">
+        <span class="header__hamburger--bar header__hamburger--bar--top"></span>
+        <span class="header__hamburger--bar header__hamburger--bar--middle"></span>
+        <span class="header__hamburger--bar header__hamburger--bar--bottom"></span>
+    </div>
+    </header>
+    <script type="module" src="/src/js/main.js"></script>
+  </body>
+</html>
