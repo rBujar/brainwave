92e2ebf7af41bd797afe6af4484538a0e26ae7e2
Bujar Roka <rokabujar@gmail.com>
2026-04-16 22:59:48 +0200

    testing fix

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import { resolve } from 'path';
 export default defineConfig({
   build: {
     outDir: 'dist',
-    manifest: true,
     assetsDir: 'assets',
+    manifest: true,
+
     rollupOptions: {
       input: {
         header: resolve(__dirname, 'src/js/header.js'),
-        // main: resolve(__dirname, 'src/js/main.js'),
         login: resolve(__dirname, 'src/js/login.js'),
         logout: resolve(__dirname, 'src/js/logout.js'),
         register: resolve(__dirname, 'src/js/register.js'),
@@ -17,20 +17,22 @@ export default defineConfig({
         contact: resolve(__dirname, 'src/js/contact.js'),
         passwordReset: resolve(__dirname, 'src/js/password-reset.js'),
         pricing: resolve(__dirname, 'src/js/pricing.js'),
-        product: resolve(__dirname, './src/js/product.js'),
+        product: resolve(__dirname, 'src/js/product.js'),
         video: resolve(__dirname, 'src/js/video.js'),
+
         style: resolve(__dirname, 'src/css/style.scss'),
       },
     },
   },
-  
-  // Don't copy public dir, we'll handle assets via imports
+
+  base: './',
+
   publicDir: false,
-  base: '/',
-  
+
   server: {
     port: 5173,
     cors: true,
+
     watch: {
       include: ['**/*.php'],
     },
