fda02a51a5efe4f16bfab5a48c543ce3c3acdb84
Meriton Pylla <meritonpylla@gmail.com>
2026-04-22 11:17:50 +0200

    Remove vertical direction from swiper configuration

--- src/js/product.js
+++ src/js/product.js
@@ -21,7 +21,6 @@ var swiper = new Swiper(".mySwiper", {
       slidesPerView: 4,
       freeMode: true,
       watchSlidesProgress: true,
-      direction: 'vertical',
     });
     var swiper2 = new Swiper(".mySwiper2", {
       spaceBetween: 40,
